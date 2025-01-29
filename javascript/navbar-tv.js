6fb0fc456bd465fa218b1a499226b075e2c83d6c
GameVerse <gameverse.site@gmail.com>
2025-01-29 16:17:54 -0700

    Update WatchVerse navbar to show profiles, and new stuff

--- javascript/navbar-tv.js
+++ javascript/navbar-tv.js
@@ -1,22 +1,45 @@
-// Dynamically add the navbar
-document.getElementById("navbar").innerHTML = `
-  <div class="navbar">
-    <div class="navbar-left">
-      <a href="/index.html">
-        <img src="/uploads/branding/favicon-watch.png" alt="Site Logo" width="10%" height="10%">
-      </a>
-      <a href="/index.html" class="gradient-green">WatchVerse</a>
-    </div>
-    <div class="navbar-right">
-      <div class="navwatch">
-        <a href="/"><i class="fa fa-home fa-lg"></i></a>
-        <a href="/games"><i class="fa fa-gamepad fa-lg"></i></a>
-        <a href="/music"><i class="fa fa-music fa-lg"></i></a>
-        <a href="/tv"><i class="fa fa-television fa-lg"></i></a>
-        <a href="/reviews"><i class="fa fa-star fa-lg"></i></a>
-        <a href="/contact"><i class="fa fa-envelope fa-lg"></i></a>
-        <a href="/share"><i class="fa fa-share-square fa-lg"></i></a>
+document.addEventListener('DOMContentLoaded', () => { // Ensure DOM is loaded before executing the script
+  // Log the logged-in user from localStorage
+  const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+  const user = loggedInUser || { username: 'Guest', photo: '/uploads/branding/favicon.png' }; // Default to guest if no user is logged in
+
+  // Render navbar with user info
+  const navbar = document.getElementById("navbar");
+  if (navbar) {
+    navbar.innerHTML = `
+      <div class="navbar">
+        <div class="navbar-left">
+          <a href="/index.html"><img src="/uploads/branding/favicon-watch.png" alt="Logo" width="10%" height="10%"></a>
+          <a href="/index.html" class="gradient-green">WatchVerse</a>
+        </div>
+        <div class="navbar-right">
+          ${user.username !== 'Guest' ? `
+            <div class="user-info">
+            <a class="user-info" style=color:white href=/editprofile>
+              <img src="${user.photo}" alt="${user.username}" class="profile-img">
+              <span>${user.username}</span></a><img height=30px width=30px src=/uploads/images/profile-verified.png alt='This is a verified GameVerse profile' title='This is a verified GameVerse profile'>
+            </div>
+          ` : '<a href="/editprofile.html" style="color:white;">Create Profile <i class="fa fa-user"></i></a>'}
+          <div class="divider"></div>
+          <div class="navwatch">
+            <a href="/"><i class="fa fa-home fa-lg"></i></a>
+            <a href="/games"><i class="fa fa-gamepad fa-lg"></i></a>
+            <a href="/music"><i class="fa fa-music fa-lg"></i></a>
+            <a href="/tv"><i class="fa fa-television fa-lg"></i></a>
+            <a href="/reviews"><i class="fa fa-star fa-lg"></i></a>
+            <a href="/contact"><i class="fa fa-envelope fa-lg"></i></a>
+            <a href="/share"><i class="fa fa-share-square fa-lg"></i></a>
+            <a href="/personalization"><i class="fa-solid fa-paintbrush"></i></a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-`;
+    `;
+  }
+  const style = document.createElement('style');
+  style.innerHTML = `
+    .user-info { display: flex; align-items: center; gap: 10px; color: white; }
+    .profile-img { width: 30px; height: 30px; border-radius: 50%; border: 2px solid #FA1228;}
+    .divider { border-left: 1px solid #fff; height: 30px; margin: 0 15px; }
+  `;
+  document.head.appendChild(style);
+});
