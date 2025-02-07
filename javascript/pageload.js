a5b83bd7b6dbf0c1351a7888756a696a431bdc53
GameVerse <gameverse.site@gmail.com>
2025-02-07 08:20:31 -0700

    Load pages to 3 secs with text roll anim

--- javascript/pageload.js
+++ javascript/pageload.js
@@ -1,7 +1,8 @@
 var myVar;
 
 function myFunction() {
-  myVar = setTimeout(showPage, 700); // Modify the number to modify load time (700+ recommended)
+  myVar = setTimeout(showPage, 3000); // Modify the number to modify load time (700+ recommended)
+  updateLoaderText(); // Call the function to update the loader text
 }
 
 function showPage() {
@@ -9,5 +10,16 @@ function showPage() {
   document.getElementById("loadertext").style.display = "none";
   document.getElementById("loader-container").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
+}
 
+function updateLoaderText() {
+  const loaderText = document.getElementById("loadertext");
+  if (loaderText) {
+    const messages = ["Starting up the servers...", "Finding entertainment...", "Organizing and optimizing..."];
+    let index = 0;
+    setInterval(() => {
+      loaderText.innerHTML = `<h3>${messages[index]}</h3>`;
+      index = (index + 1) % messages.length;
+    }, 1000); // Change message every second
+  }
 }
