47946267d879bce89d4a9d14fdb3f8c23e6d4191
Tanner <tanner.ordonez@gmail.com>
2025-02-19 21:10:38 -0700

    update panic key

--- javascript/panic.js
+++ javascript/panic.js
@@ -2,6 +2,6 @@ const panickey = "`";
 const panicURL = "https://www.google.com/search?q=calculator"; // Suggestion: make this what the cloak is set to
 document.addEventListener("keydown", function (event) {
   if (event.key === panickey) {
-    window.location.href = "https://www.google.com";
+    window.location.href = panicURL;
   }
 });
