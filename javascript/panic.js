44122595faa1b1ef6802f6cc411e934580c2044d
Tanner Ordonez <107821706+Tan01010@users.noreply.github.com>
2025-02-20 10:54:42 -0700

    Update panic.js

--- javascript/panic.js
+++ javascript/panic.js
@@ -1,5 +1,5 @@
 const panickey = "`";
-const panicURL = cloakLink; // Suggestion: make this what the cloak is set to
+const panicURL = localStorage.getItem("cloakLink"); // Suggestion: make this what the cloak is set to
 document.addEventListener("keydown", function (event) {
   if (event.key === panickey) {
     window.location.href = panicURL;
