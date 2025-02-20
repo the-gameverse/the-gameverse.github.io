742002c9ce4b08b0108dcc9d3463523e9b0a8ad9
Tanner <tanner.ordonez@gmail.com>
2025-02-19 21:10:21 -0700

    Update Easter Egg & Add panic key

---
+++ javascript/panic.js
@@ -0,0 +1,7 @@
+const panickey = "`";
+const panicURL = "https://www.google.com/search?q=calculator"; // Suggestion: make this what the cloak is set to
+document.addEventListener("keydown", function (event) {
+  if (event.key === panickey) {
+    window.location.href = "https://www.google.com";
+  }
+});
