6d6e953de965e4d6fc0b5f2d3f3f2fb9098f1699
Tanner <tanner.ordonez@gmail.com>
2025-02-20 18:08:26 -0700

    Add panic key configuration and anti-tab close feature in settings

--- javascript/panic.js
+++ javascript/panic.js
@@ -1,4 +1,4 @@
-const panickey = "`";
+const panickey = localStorage.getItem("panicKey"); // Suggestion: make this "Escape"
 const panicURL = localStorage.getItem("cloakLink"); // Suggestion: make this what the cloak is set to
 document.addEventListener("keydown", function (event) {
   if (event.key === panickey) {
