22088f2da8fb75d7f112f7142bd515137145788c
Tanner <tanner.ordonez@gmail.com>
2025-02-20 20:11:57 -0700

    Update aboutBlank function to open a new blank window instead of a new tab

--- javascript/cloaking.js
+++ javascript/cloaking.js
@@ -33,7 +33,7 @@ const cloak = {
   },
   aboutBlank(url) {
     if (!url) url = "https://www.google.com/search?q=how+many+seconds+in+a+day";
-    const newWindow = window.open("chrome://newtab", "_blank");
+    const newWindow = window.open("", "_blank");
     const iframe = newWindow.document.createElement("iframe");
     newWindow.document.body.style.margin = "0";
     newWindow.document.body.style.height = "100vh";
