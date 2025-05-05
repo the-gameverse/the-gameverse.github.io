755484828212f8ab22a9a6ec2b3240443d79b1c7
Ryan Lamb <ryanlamb749@gmail.com>
2025-05-05 10:51:05 -0600

    Update navbar.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <div class="nav-links">
           <a href="/"><i class="fa fa-home fa-lg"></i></a>
           <a href="/games"><i class="fa fa-gamepad fa-lg"></i></a>
-          <a href="/music"><i class="fa fa-music fa-lg"></i></a>
+          <a href="/apps"><i class="fa fa-cube fa-lg"></i></a>
           <a href="/tv"><i class="fa fa-television fa-lg"></i></a>
           <a href="/blog"><i class="fa fa-comment-alt fa-lg"></i></a>
           <a href="/settings"><i class="fa fa-gear fa-lg"></i></a>
